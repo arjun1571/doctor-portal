f7fec95b2a480b451ece930d19e4ee8befb3204f
Arjun <arjundasback515@gmail.com>
2023-04-12 12:18:02 +0600

    home page cardInfo section done

---
+++ src/pages/Home/CardInfo/CardInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CardInfo = ({card}) => {
+    const {name,des,logo,bg}=card;
+    return (
+        <div className={`card card-side ${bg} shadow-xl text-white p-4`}>
+            <figure><img src={logo} alt="Movie"/></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{des}</p>
+
+            </div>
+        </div>
+    );
+};
+
+export default CardInfo;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import CardsInfo from '../CardInfo/CardsInfo';
 
 const Home = () => {
     return (
         <div className=''>
             <Banner></Banner>
+            <CardsInfo></CardsInfo>
         </div>
     );
 };
