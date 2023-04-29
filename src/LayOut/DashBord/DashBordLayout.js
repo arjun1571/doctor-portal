d6a990076a2b436466355fe04478e885303f5575
Arjun <arjundasback515@gmail.com>
2023-04-29 12:16:38 +0600

    side drawer button added on navbar on movile screen

--- src/LayOut/DashBord/DashBordLayout.js
+++ src/LayOut/DashBord/DashBordLayout.js
@@ -6,7 +6,21 @@ const DashBordLayout = () => {
     return (
         <div>
             <NavBar></NavBar>
-            <Outlet></Outlet>
+            <div className="drawer drawer-mobile">
+                <input id="dashbord-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col items-center ">
+                    <Outlet></Outlet>
+                </div> 
+                <div className="drawer-side">
+                    <label htmlFor="dashbord-drawer" className="drawer-overlay"></label> 
+                    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+
+                    <li><a>Sidebar Item 1</a></li>
+                    <li><a>Sidebar Item 2</a></li>
+                    </ul>
+                
+                </div>
+            </div>
         </div>
     );
 };
--- src/SheardPage/NavBar/NavBar.js
+++ src/SheardPage/NavBar/NavBar.js
@@ -60,6 +60,22 @@ const NavBar = () => {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navItem}</ul>
       </div>
+      <label htmlFor="dashbord-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
     </div>
   );
 };
